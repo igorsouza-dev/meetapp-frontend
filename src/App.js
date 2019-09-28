47fb0253f7c10fc2938d8d10fc3952c90d5a5e49
Igor Souza <igorsouza.dev@gmail.com>
2019-09-28 17:42:17 -0300

    Adding layouts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Router } from 'react-router-dom';
+import './config/ReactotronConfig';
+import GlobalStyle from './styles/globals';
+import Routes from './routes';
+import history from './services/history';
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router history={history}>
+      <GlobalStyle />
+      <Routes />
+      <ToastContainer autoClose={3000} />
+    </Router>
+  );
 }
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Route from './Route';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
+      <Route path="/dashboard" exact component={Dashboard} />
+    </Switch>
+  );
+}
